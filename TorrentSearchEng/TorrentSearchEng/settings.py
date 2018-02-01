23a7862fb101a7385c4ec1875f5f85f4ab3357f2
MdJeweleIslam <mdjeweleislam@gmail.com>
2018-02-02 01:58:06 +0600

    Setup all required things

--- TorrentSearchEng/TorrentSearchEng/settings.py
+++ TorrentSearchEng/TorrentSearchEng/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #App Install
+    'TSearch',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'TorrentSearchEng.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
